d1016bcd42190aa664057eab53ebd30ae6e4c7bb
phothinh <phothinh01@gmail.com>
2024-02-01 21:33:43 +0100

    remake UI

--- SentimentAnalysis/ContentView.swift
+++ SentimentAnalysis/ContentView.swift
@@ -12,84 +12,100 @@ enum Sentiment: String {
     case negative = "NEGATIVE"
     case mixed = "MIXED"
     case neutral = "NEUTRAL"
-    
+}
+
+extension Sentiment {
     func getColor() -> Color {
-        switch(self) {
+        switch (self) {
         case .positive:
-            return Color.green
+            return .green
         case .negative:
-            return Color.red
+            return .red
         case .mixed:
-            return Color.purple
+            return .purple
         case .neutral:
-            return Color.gray
+            return .gray
         }
     }
     
     func getEmoji() -> String {
-        switch(self) {
+        switch (self) {
         case .positive:
-            return "emoji content"
+            return "😀"
         case .negative:
-            return "emoji pas content"
+            return "👹"
         case .mixed:
-            return "emoji réfléchissant"
+            return "🥴"
         case .neutral:
-            return "emoji neutre"
+            return "🫥"
         }
     }
 }
 
-
-
 struct ContentView: View {
     @State var modelInput: String = ""
-    @State var outputSentiment: Sentiment?
+    @State var modelOutput: String = ""
+    @State var outputSentiment: Sentiment? = nil
     
     var body: some View {
         NavigationStack {
             VStack {
-                VStack {
-                    Text("Entrez une phrase, l'IA va deviner votre sentiment")
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Entrez votre phrase, l'IA s'occupe du reste")
                         .fontWeight(.bold)
-                        .fontDesign(.rounded)
+                        .fontDesign(.monospaced)
                         .foregroundStyle(.white)
                     
-                    TextEditor(text: $modelInput)
-                        .onChange(of: modelInput, { oldValue, newValue in
-                            outputSentiment = nil
-                        }).frame(height: 100)
-                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                    TextEditor(text: $modelInput).onChange(of: modelInput, {
+                        oldValue, newValue in outputSentiment = nil
+                    }).frame(height: 100)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                     
-                    Button {
-                        
-                    } label: {
-                        Text("Deviner le sentiment")
-                    }.padding()
+                    Button(action: {
+                        classify()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            Text("Deviner le sentiment")
+                        }.padding()
+                    })
+                    .buttonStyle(BorderedProminentButtonStyle())
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
                     
-                }
-                .padding()
-                .background(Color.purple.gradient)
-                
-                
+                }.padding()
+                    .background(Color.purple.gradient.opacity(0.9))
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
                 
                 VStack {
-//                    Text(outputSentiment?.getEmoji() ?? "")
+                    Text(outputSentiment?.getEmoji() ?? "")
                     Text(outputSentiment?.rawValue ?? "")
-                }.padding()
-                .background(Color.blue.gradient)
-
-                
+                }.frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(outputSentiment?.getColor().gradient ?? Color.blue.gradient)
+                    .clipShape(RoundedRectangle(cornerRadius: 18.0))
+                    .opacity(outputSentiment == nil ? 0 : 1.0)
+                    .scaleEffect(outputSentiment == nil ? 0.3 : 1.0)
+                    .animation(.bouncy, value: outputSentiment)
+                Spacer()
             }
-            .navigationTitle("IA du futur")
-            
-            
+            .navigationTitle("🧠 IA du football")
+            .padding()
         }
-        
+    }
+    
+    func classify() {
+        do {
+            // MyModel est une classe générée automatiquement par Xcode
+            let model = try SentimentAnalysis_1(configuration: .init())
+            let prediction = try model.prediction(text: modelInput)
+            modelOutput = prediction.label
+            outputSentiment = Sentiment(rawValue: modelOutput)
+        } catch {
+            modelOutput = "Something went wrong"
+        }
     }
 }
-
-
 
 #Preview {
     ContentView()
